5d27c71ac0701a81e36f8e18ef86e69286e728ed
adilvelizade <adilvalizada@outlook.com>
2022-05-11 18:09:06 +0400

    task is done

---
+++ index.js
@@ -0,0 +1,72 @@
+const draggableItems = document.querySelectorAll(".draggable-item");
+const dropZones = document.querySelectorAll(".dropzone");
+
+let innerX, innerY;
+
+draggableItems.forEach((draggableItem, index) => {
+  draggableItem.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData(`draggableItemId`, event.target.id);
+    innerX = event.offsetX;
+    innerY = event.offsetY;
+  });
+  draggableItem.addEventListener("mousedown", () => {
+    draggableItem.style.zIndex = "1";
+    draggableItems.forEach((item, i) => {
+      if (i !== index) {
+        draggableItems[i].style.zIndex = "0";
+      }
+    });
+  });
+});
+
+dropZones.forEach((dropZone, index) => {
+  dropZone.addEventListener("dragover", (event) => {
+    event.preventDefault();
+  });
+});
+
+dropZones.forEach((dropZone, index) => {
+  dropZone.addEventListener("drop", (event) => {
+    const draggableItemId = event.dataTransfer.getData("draggableItemId");
+    const draggableItem = document.querySelector(`#${draggableItemId}`);
+    draggableItem.style.position = "absolute";
+
+    let offsetX = event.offsetX - innerX + "px";
+    let offsetY = event.offsetY - innerY + "px";
+
+    if (offsetX !== event.offsetX && offsetY !== event.offsetY) {
+      console.log(event.offsetX, event.offsetY);
+    }
+
+    if (event.offsetX > 320 && event.offsetY < 80) {
+      draggableItem.style.top = "0px";
+      draggableItem.style.left = "320px";
+    } else if (event.offsetY > 320 && event.offsetX < 80) {
+      draggableItem.style.top = "320px";
+      draggableItem.style.left = "0px";
+    } else if (event.offsetY > 320 && event.offsetX > 320) {
+      draggableItem.style.top = "320px";
+      draggableItem.style.left = "320px";
+    } else if (event.offsetY < 80 && event.offsetX < 80) {
+      draggableItem.style.top = "0px";
+      draggableItem.style.left = "0px";
+    } else if (event.offsetY < 80) {
+      draggableItem.style.top = "0px";
+      draggableItem.style.left = offsetX;
+    } else if (event.offsetX < 80) {
+      draggableItem.style.top = offsetY;
+      draggableItem.style.left = "0px";
+    } else if (event.offsetY > 320) {
+      draggableItem.style.top = "320px";
+      draggableItem.style.left = offsetX;
+    } else if (event.offsetX > 320) {
+      draggableItem.style.top = offsetY;
+      draggableItem.style.left = "320px";
+    } else {
+      draggableItem.style.left = offsetX;
+      draggableItem.style.top = offsetY;
+    }
+
+    event.target.appendChild(draggableItem);
+  });
+});
